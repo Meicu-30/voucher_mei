27ce65757c552f3ece19f690f4d37f44da8c76bc
Meilani <meilanichui@gmail.com>
2025-07-29 22:25:59 -0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: ' voucer_meii',
+  webDir: 'www'
+};
+
+export default config;
